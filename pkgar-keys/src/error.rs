faaf1c88baa399e05288adb04a1f05a08b4c8432
Wesley Hershberger <mggmugginsmc@gmail.com>
2020-08-16 13:51:43 -0400

    pkgar-keys: Refactor Errors
    Fixed two little bugs:
    - Private key files now use 600 instead of 644
    - Fixed default key file paths
    
    Pulled in the same error deps as the main library,
    - UFE for easy pretty-printing of errors
    - thiserror for deriving std::error::Error for error types
    
    pkgar-keys now uses two error types, one that includes a file path for
    context and an ErrorKind for passing around internally and containing
    other error types. I'm still not happy with the sheer volume of
    boilerplate needed to create Error, so more work is needed, but error
    reporting is much better with these changes, and is probably more
    maintainable even given the boilerplate.

--- pkgar-keys/src/error.rs
+++ pkgar-keys/src/error.rs
@@ -1,85 +1,46 @@
-use std::error::Error as StdError;
-use std::fmt::{self, Display};
 use std::io;
 use std::path::PathBuf;
 
-#[derive(Debug)]
-pub struct FileError {
-    kind: Error,
-    src: PathBuf,
+use user_error::UFE;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+#[error("File: {path}")]
+pub struct Error {
+    #[source]
+    pub src: ErrorKind,
+    pub path: PathBuf,
 }
 
-impl Display for FileError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", self.src.display(), self.kind)
-    }
-}
+impl UFE for Error {}
 
-impl StdError for FileError {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        self.kind.source()
-    }
-}
-
-#[derive(Debug)]
-pub enum Error {
-    Custom(String),
-    Io(io::Error),
+#[derive(Debug, Error)]
+pub enum ErrorKind {
+    #[error("Io")]
+    Io(#[from] io::Error),
+    
+    #[error("Key length invalid")]
     KeyInvalid,
+    
+    #[error("Public and secret keys do not match")]
     KeyMismatch,
-    MAlloc,
+    
+    #[error("Unable to allocate locked/zeroed memory")]
+    SecureMem,
+    
+    #[error("Invalid nonce length")]
     NonceInvalid,
+    
+    #[error("Incorrect passphrase")]
     PassphraseIncorrect,
+    
+    #[error("Passphrases did not match")]
     PassphraseMismatch,
-    Ser(toml::ser::Error),
-    Deser(toml::de::Error),
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
-
-impl From<toml::de::Error> for Error {
-    fn from(err: toml::de::Error) -> Error {
-        Error::Deser(err)
-    }
-}
-
-impl From<toml::ser::Error> for Error {
-    fn from(err: toml::ser::Error) -> Error {
-        Error::Ser(err)
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let msg = match self {
-            Error::Custom(e) => e.clone(),
-            Error::KeyInvalid => "Key length invalid".to_string(),
-            Error::KeyMismatch => "Public and private keys do not match".to_string(),
-            Error::MAlloc => "Unable to allocate locked/zeroed memory".to_string(),
-            Error::NonceInvalid => "Nonce length invalid".to_string(),
-            Error::PassphraseIncorrect => "Incorrect passphrase".to_string(),
-            Error::PassphraseMismatch => "Passphrases do not match".to_string(),
-            Error::Io(err) => format!("{}", err),
-            Error::Ser(err) => format!("{}", err),
-            Error::Deser(err) => format!("{}", err),
-        };
-        
-        write!(f, "Error: {}", msg)
-    }
-}
-
-impl StdError for Error {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match self {
-            Error::Io(err) => Some(err),
-            Error::Ser(err) => Some(err),
-            Error::Deser(err) => Some(err),
-            _ => None,
-        }
-    }
+    
+    #[error("Serialization")]
+    Ser(#[from] toml::ser::Error),
+    
+    #[error("Deserialization")]
+    Deser(#[from] toml::de::Error),
 }
 
