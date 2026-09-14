aee5d37d6bf7c40fd043f86fe1e5117eac772e4d
Wildan M <willnode@wellosoft.net>
2026-09-14 23:34:30 +0700

    Update clap

--- pkgar-keys/src/main.rs
+++ pkgar-keys/src/main.rs
@@ -3,89 +3,116 @@ use std::io;
 use std::path::PathBuf;
 use std::process;
 
-use clap::clap_app;
+use clap::{Arg, ArgAction, Command, value_parser};
+use clap::{crate_authors, crate_description, crate_name, crate_version};
 
 use pkgar_keys::{
     DEFAULT_PUBKEY, DEFAULT_SECKEY, Error, SecretKeyFile, gen_keypair, get_skey, re_encrypt,
 };
 
 fn cli() -> Result<i32, Error> {
-    let matches = clap_app!(("pkgar-keys") =>
-        (author: "Wesley Hershberger <mggmugginsmc@gmail.com>")
-        (about: "NaCl key management for pkgar")
-        (@arg skey: -s --skey [FILE] +global "Alternate secret keyfile (defaults to '~/.pkgar/keys/id_ed25519.toml')")
-        (@setting SubcommandRequired)
-        (@subcommand gen =>
-            (about: "Generate a keypair and store on the filesystem")
-            (@arg pkey: -p --pkey [FILE]
-                "Alternate public keyfile (defaults to `~/.pkgar/keys/id_ed25519.pub.toml`)")
-            (@arg plaintext:  -P --plaintext
-                "Do not prompt for a passphrase and store the secret key as plain text")
-            (@arg force:      -f --force
-                "Don't check for existing files before generating a new keypair")
-        )
-        (@subcommand rencrypt =>
-            (about: "Re-encrypt the secret key provided by --skey")
-        )
-        (@subcommand export =>
-            (about: "Print the public key corresponding to the key given with --skey in the pkgar pubkey format")
-            (@arg file: -f --file [FILE] "Output to a file instead of stdout")
-        )
-    ).get_matches();
+    let skey = Arg::new("skey")
+        .short('s')
+        .long("skey")
+        .value_name("FILE")
+        .help("Alternate secret keyfile")
+        .global(true)
+        .value_parser(value_parser!(PathBuf))
+        .default_value_os(DEFAULT_SECKEY.as_os_str());
 
-    let skey_path = matches
-        .value_of("skey")
-        .map(PathBuf::from)
-        .unwrap_or(DEFAULT_SECKEY.clone());
+    let pkey = Arg::new("pkey")
+        .short('p')
+        .long("pkey")
+        .value_name("FILE")
+        .help("Alternate public keyfile")
+        .value_parser(value_parser!(PathBuf))
+        .default_value_os(DEFAULT_PUBKEY.as_os_str());
 
-    let (subcommand, submatches) = matches.subcommand();
-    let submatches = submatches.expect("A subcommand should have been provided");
+    let r#gen = Command::new("gen")
+        .about("Generate a keypair and store on the filesystem")
+        .arg(pkey)
+        .arg(
+            Arg::new("plaintext")
+                .short('P')
+                .long("plaintext")
+                .help("Do not prompt for a passphrase and store the secret key as plain text")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("force")
+                .short('f')
+                .long("force")
+                .help("Don't check for existing files before generating a new keypair")
+                .action(ArgAction::SetTrue),
+        );
+    let reencrypt = Command::new("rencrypt").about("Re-encrypt the secret key provided by --skey");
+    let export = Command::new("export")
+        .about("Print the public key from --skey in the pkgar pubkey format")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .value_parser(value_parser!(PathBuf))
+                .help("Output to a file instead of stdout"),
+        );
 
-    match subcommand {
-        "gen" => {
+    let matches = Command::new(crate_name!())
+        .author(crate_authors!(", "))
+        .about(crate_description!())
+        .version(crate_version!())
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .arg(skey)
+        .subcommand(r#gen)
+        .subcommand(reencrypt)
+        .subcommand(export)
+        .get_matches();
+
+    let skey_path = matches.get_one::<PathBuf>("skey").unwrap();
+
+    match matches.subcommand() {
+        Some(("gen", submatches)) => {
             if let Some(keydir) = skey_path.parent() {
-                fs::create_dir_all(&keydir).map_err(|source| Error::Io {
+                fs::create_dir_all(keydir).map_err(|source| Error::Io {
                     source,
                     path: Some(keydir.to_path_buf()),
                     context: "Creating directory",
                 })?;
             }
 
-            if !submatches.is_present("force") && skey_path.exists() {
+            if !submatches.get_flag("force") && skey_path.exists() {
                 return Err(Error::Io {
                     source: std::io::Error::from(std::io::ErrorKind::AlreadyExists),
-                    path: Some(skey_path),
+                    path: Some(skey_path.clone()),
                     context: "Key already exist",
                 });
             }
 
-            let pkey_path = submatches
-                .value_of("pkey")
-                .map(PathBuf::from)
-                .unwrap_or(DEFAULT_PUBKEY.clone());
+            let pkey_path = submatches.get_one::<PathBuf>("pkey").unwrap();
 
-            if !submatches.is_present("plaintext") {
-                gen_keypair(&pkey_path, &skey_path)?;
+            if !submatches.get_flag("plaintext") {
+                gen_keypair(pkey_path, skey_path)?;
             } else {
                 let (pkey, skey) = SecretKeyFile::new();
-                pkey.save(&pkey_path)?;
-                skey.save(&skey_path)?;
+                pkey.save(pkey_path)?;
+                skey.save(skey_path)?;
             }
         }
-        "export" => {
-            let skey = get_skey(&skey_path)?;
+        Some(("export", submatches)) => {
+            let skey = get_skey(skey_path)?;
             let pkey = skey
                 .public_key_file()
                 .expect("Secret key was encrypted after being decrypted");
 
-            if let Some(file) = submatches.value_of("file") {
+            if let Some(file) = submatches.get_one::<PathBuf>("file") {
                 pkey.save(file)?;
             } else {
                 pkey.write(io::stdout().lock())?;
             }
         }
-        "rencrypt" => {
-            re_encrypt(&skey_path)?;
+        Some(("rencrypt", _)) => {
+            re_encrypt(skey_path)?;
             println!("Successfully re-encrypted {}", skey_path.display());
         }
         _ => unreachable!(),
